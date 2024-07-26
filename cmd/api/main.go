ce3e0995064eb169187bf3c39880f6457e1262e4
Marcello Fabrizio <marcfabrizio10@gmail.com>
2024-07-25 21:32:01 -0300

    Separete main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,57 +1,16 @@
-// Package main ...
 package main
 
 import (
 	"fmt"
-
-	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/proto"
-	"github.com/spf13/afero"
+	"go-make-me-a-pdf/internal/server"
 )
 
 func main() {
-	rod_pod()
-}
+	server := server.NewServer()
 
-func rod_pod() {
-	page := rod.New().MustConnect().MustPage("https://www.olhonocarro.com.br/resultado-da-consulta/?queryCode=100&queryId=668c36793d0a5db6151a2296").MustWaitDOMStable() //.MustPDF("sample.pdf")
-
-	res := page.MustEval(`() => document.body.scrollHeight`)
-
-	scrollHeight := res.Num()
-
-	fmt.Println("scrollHeight", scrollHeight)
-
-	marginTop := 0.0
-	marginBottom := 0.0
-
-	reportPdf, err := page.PDF(&proto.PagePrintToPDF{
-		MarginBottom:    &marginBottom,
-		MarginTop:       &marginTop,
-		PrintBackground: true,
-	})
-
+	fmt.Println("Sever running at port", server.Addr)
+	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
-
-	fs := afero.NewOsFs()
-
-	bin, err := afero.ReadAll(reportPdf)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	err = afero.WriteFile(fs, "report.pdf", bin, 0644)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("wrote sample.pdf")
-
 }
